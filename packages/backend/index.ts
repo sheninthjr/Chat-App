b40d9059a174875520005ffbd6ac2566d318e2a1
sheninthjr <sheninthjr23@gmail.com>
2023-11-19 14:39:13 +0530

    Websocket initial Backend

---
+++ packages/backend/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const port = 3000;
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", async (ws, req) => {
+    ws.on("message", (message) => {
+        console.log("received: %s", message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+});
+
+server.listen(port);
