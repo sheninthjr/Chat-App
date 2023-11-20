95658cc2a33843e48f20ea87b1b7aedc855a6ee4
sheninthjr <sheninthjr23@gmail.com>
2023-11-20 21:53:44 +0530

    Nextjs Added

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -9,10 +9,37 @@ const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 
+const users: { [key: string]: {
+    room: string;
+    ws: any;
+} } = {};
+
+let counter = 0;
+
 wss.on("connection", async (ws, req) => {
-    ws.on("message", (message) => {
-        console.log("received: %s", message);
-        ws.send(`Hello, you sent -> ${message}`);
+    const wsId = counter++;
+    ws.on("message", (message: string) => {
+        const data = JSON.parse(message.toString());
+        if (data.type === "join") {
+            users[wsId] = {
+                room: data.payload.roomId,
+                ws
+            };
+        }
+        if (data.type === "message") {
+            const roomId = users[wsId].room;
+            const message = data.payload.message;
+            Object.keys(users).forEach((wsId) => {
+                if (users[wsId].room === roomId) {
+                    users[wsId].ws.send(JSON.stringify({
+                        type: "message",
+                        payload: {
+                            message
+                        }
+                    }));
+                }
+            })
+        }
     });
 });
 
