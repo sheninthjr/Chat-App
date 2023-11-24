9de440d96e7e2963256c35241fd2a5663f434264
sheninthjr <sheninthjr23@gmail.com>
2023-11-24 23:03:10 +0530

    Link tag added

--- packages/ui/components/NavBar.tsx
+++ packages/ui/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
     <div>
       <div className="bg-black h-16 p-4 fixed top-0 w-full z-10 shadow-md shadow-slate-700">
         <div className="flex justify-between">
-          <div className="flex font-semibold text-lg">Sheninth Jr ChatApp</div>
+          <div className="flex font-semibold text-lg"><Link href="https://chat.sheninthjr.com">Sheninth Jr Chat App</Link></div>
         </div>
       </div>
     </div>
