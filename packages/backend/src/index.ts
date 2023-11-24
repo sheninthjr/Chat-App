541ea526ee0055a277ea332cefe26bbe47d092fb
sheninthjr <sheninthjr23@gmail.com>
2023-11-24 22:00:23 +0530

    Multicore

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import http from "http";
+import cluster from 'cluster';
+import os from 'os';
 import { WebSocketServer } from "ws";
 import {RedisSubscriptionManager} from "./RedisClient";
 
@@ -17,28 +19,37 @@ const users: { [key: string]: {
 
 let counter = 0;
 
-wss.on("connection", async (ws, req) => {
-    const wsId = counter++;
-    ws.on("message", (message: string) => {
-        const data = JSON.parse(message.toString());
-        if (data.type === "join") {
-            users[wsId] = {
-                room: data.payload.roomId,
-                ws
-            };
-            RedisSubscriptionManager.getInstance().subscribe(wsId.toString(), data.payload.roomId, ws);
-        }
-        if (data.type === "message") {
-            const roomId = users[wsId].room;
-            const message = data.payload.message;
-            RedisSubscriptionManager.getInstance().addChatMessage(roomId,wsId,message);
-        }
-    });
-    ws.on("disconnect", () => {
-        RedisSubscriptionManager.getInstance().unsubscribe(wsId.toString(), users[wsId].room);
-    })
-});
+const cpuCount = os.cpus().length;
 
-server.listen(port,()=>{
-    console.log(`Server Listening on port:${port}`)
-});
+if(cluster.isMaster){
+    for (let i=0;i<cpuCount;i++){
+        cluster.fork();
+    }
+}else{
+    wss.on("connection", async (ws, req) => {
+        const wsId = counter++;
+        ws.on("message", (message: string) => {
+            const data = JSON.parse(message.toString());
+            if (data.type === "join") {
+                users[wsId] = {
+                    room: data.payload.roomId,
+                    ws
+                };
+                RedisSubscriptionManager.getInstance().subscribe(wsId.toString(), data.payload.roomId, ws);
+            }
+            if (data.type === "message") {
+                const roomId = users[wsId].room;
+                const message = data.payload.message;
+                RedisSubscriptionManager.getInstance().addChatMessage(roomId,wsId,message);
+            }
+        });
+        ws.on("disconnect", () => {
+            RedisSubscriptionManager.getInstance().unsubscribe(wsId.toString(), users[wsId].room);
+        })
+    });
+    
+    server.listen(port,()=>{
+        console.log(`Server Listening on port:${port}`)
+    });
+}
+
